f32f41134fe759b4fe026ff58eb870216ab854d4
Nkaemmerlen <nathankaemmerlen@yahoo.com>
2022-04-14 10:26:15 -0500

    add styling to index and login pages

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,5 +1,3 @@
-# myapp/__init__.py 
-
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
 import os
@@ -12,12 +10,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-############################
-###### DATABASE SETUP ######
-############################
 
 
-# set up connection to db
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost:5432/myjob"
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL').replace("://", "ql://", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,9 +23,6 @@ db.init_app(app)
 Migrate(app, db)
 
 
-#############################
-
-############ LOGIN CONFIGS ##############
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -39,17 +32,17 @@ login_manager.login_view = 'users.login'
 
 
 
-# Registering Blueprints - 
+
 
 from myapp.core.views import core 
 app.register_blueprint(core)
 
-#linking the 404 and 403 error pages into the app 
+
 from myapp.error_pages.handlers import error_pages
 app.register_blueprint(error_pages)
 
 
-#linking users views Blueprint
+
 from myapp.users.views import users
 app.register_blueprint(users)
 
